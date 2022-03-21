a3dcc9694ca75cebeba919489c427114b9fdb7fa
dominik2323 <dominik.tomcik23@gmail.com>
2022-03-21 13:47:00 +0100

    inline-script

---
+++ inlineCss.js
@@ -0,0 +1,18 @@
+var inlineCss = require("inline-css");
+const fs = require("fs");
+
+fs.readFile("index.html", "utf8", (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  inlineCss(data, { url: `file://${__dirname}/` })
+    .then(function (html) {
+      console.log(html);
+      fs.writeFile("index.inlined-css.html", html, (err) => {
+        console.log(err);
+      });
+    })
+    .catch((e) => console.log(e));
+});
